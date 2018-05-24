b9f404495fe5047fc827e5a911a38849233009d4
Arman <arman.aghabekyan@gmail.com>
2018-05-25 02:37:43 +0500

    Adding slashes after base_dir

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -23,7 +23,7 @@ const BASE_DIR = __DIR__ . DS . '..';
 /**
  * Vendor directory.
  */
-const VENDOR_DIR = BASE_DIR . 'vendor';
+const VENDOR_DIR = BASE_DIR . DS . 'vendor';
 
 /**
  * Framework Core directory.
@@ -43,12 +43,12 @@ const LIBRARIES_DIR = CORE_DIR . DS . 'Libraries';
 /**
  * Modules directory.
  */
-const MODULES_DIR = BASE_DIR . 'modules';
+const MODULES_DIR = BASE_DIR . DS . 'modules';
 
 /**
  * Public directory.
  */
-const PUBLIC_DIR = BASE_DIR . 'public';
+const PUBLIC_DIR = BASE_DIR . DS . 'public';
 
 /**
  * Upload directory.
