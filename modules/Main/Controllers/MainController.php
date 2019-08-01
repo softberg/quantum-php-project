2ac56bd0d9537072073b0960bea2fd78bd3c04fb
Arman <arman.aghabekyan@gmail.com>
2019-08-02 02:33:36 +0400

    Using ServiceFactory and ViewFactory dependencies

--- modules/Main/Controllers/MainController.php
+++ modules/Main/Controllers/MainController.php
@@ -5,58 +5,73 @@ namespace Modules\Main\Controllers;
 use Quantum\Mvc\Qt_Controller;
 use Quantum\Http\Request;
 use Quantum\Http\Response;
-use Quantum\Factory\Factory;
+use Quantum\Factory\ServiceFactory;
+use Quantum\Factory\ViewFactory;
 use Quantum\Libraries\Lang\Lang;
 use Modules\Main\Services\Post;
 
 class MainController extends Qt_Controller {
 
+    public $view;
+    
+    public $service;
+
     public $csrfVerification = false;
-
-    public function __construct() {
-        $this->setLayout('layouts/main');
-    }
-
-    public function __before() {
+    
+    public function __before(ServiceFactory $service, ViewFactory $view) 
+    {
+        $this->view = $view;
+        
+        $this->service = $service;
+        
         $lang = Request::getSegment(1);
         Lang::init($lang);
+        
+        $this->view->setLayout('layouts/main');
     }
 
-    public function index() {
-        $this->render('index');
+    public function index() 
+    {
+        $this->view->render('index');
     }
 
-    public function about() {
-        $this->render('about');
+    public function about() 
+    {
+        $this->view->render('about');
     }
 
-    public function getPosts(Factory $factory) {
-        $postModel = $factory->getService(Post::class);
-        $posts = $postModel->getPosts();
-        $this->render('post/post', ['posts' => $posts]);
+    public function getPosts() 
+    {
+        $postService = $this->service->get(Post::class);
+        $posts = $postService->getPosts();
+        
+        $this->view->render('post/post', ['posts' => $posts]);
     }
 
-    public function getPost(Factory $factory, $id) {
-        $postModel = $factory->getService(Post::class);
-        $post = $postModel->getPost($id);
-        $this->render('post/single', ['id' => $id, 'post' => $post]);
+    public function getPost($id) 
+    {
+        $postService = $this->service->get(Post::class);
+        $post = $postService->getPost($id);
+        
+        $this->view->render('post/single', ['id' => $id, 'post' => $post]);
     }
 
-    public function amendPost(Request $request, Factory $factory, $id = null) {
-        $postModel = $factory->getService(Post::class);
+    public function amendPost(Request $request, $id = null) 
+    {
+        $postService = $this->service->get(Post::class);
         $post = [
             'title' => 'Mickey Mouse',
             'content' => 'Mickey Mouse is a cartoon character who has become an icon for the Walt Disney Company. Mickey Mouse is short for Mitchell Mouse. It was created in 1928 by Walt Disney and Ub Iwerks and voiced by Walt Disney.'
         ];
 
         if ($id) {
-            $postModel->updatePost($id, $post);
+            $postService->updatePost($id, $post);
         } else {
-            $postModel->addPost($post);
+            $postService->addPost($post);
         }
 
-        $posts = $postModel->getPosts();
-        $this->render('post/post', ['posts' => $posts]);
+        $posts = $postService->getPosts();
+        $this->view->render('post/post', ['posts' => $posts]);
     }
 
 }
