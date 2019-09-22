a6ce54d157ce8a6b2b7947044ee00edc810217bb
Arman <armanist@mail.ru>
2019-09-22 23:07:51 +0400

    Correcting demo middlewares according to new Response injection requirement

--- modules/Main/Middlewares/Editor.php
+++ modules/Main/Middlewares/Editor.php
@@ -4,16 +4,17 @@ namespace Modules\Main\Middlewares;
 
 use Modules\Main\Models\User;
 use Quantum\Middleware\Qt_Middleware;
+use Quantum\Http\Response;
 use Quantum\Http\Request;
 
 class Editor extends Qt_Middleware {
 
-    public function apply(Request $request, \Closure $next) {
+    public function apply(Request $request, Response $response, \Closure $next) {
         if(!User::isEditor()){
             redirect(base_url());
         }
         
-        return $next($request);
+        return $next($request, $response);
     }
     
 }
