9832ede73f1d6851fed7fa6435823d2316adcd1a
Arman <armanist@mail.ru>
2019-12-30 00:06:41 +0400

    Removing Main module and other clean ups

--- modules/Main/Views/lang/en/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'description' => 'Very fast and extremely simple PHP MVC framework of the next generation with a modular structure that allows you to create any projects.',
-    'installation' => 'Installation',
-    'learn_more' => 'Learn More',
-];
