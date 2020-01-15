df147878ce532dec408cb5913217255bd8bfaf79
Arman <armanist@mail.ru>
2020-01-15 16:25:57 +0400

    Removing View folder from API

--- modules/Api/Views/partials/navbar.php
+++
@@ -1,9 +0,0 @@
-<ul class="navbar">
-    <li><a class="<?php echo current_action() == 'index' ? 'active' : '' ?>" href="<?php echo base_url() ?>">Home</a></li>
-    <li><a class="<?php echo current_action() == 'getPosts' || current_action() == 'getPost' ? 'active' : '' ?>" href="<?php echo base_url() ?>/posts">Posts</a></li>
-    <?php if (auth()->user()->role == 'admin' || auth()->user()->role == 'editor'): ?>
-        <li><a class="<?php echo current_action() == 'amendPost' ? 'active' : '' ?>" href="<?php echo base_url() ?>/post/amend">New Post</a></li>
-    <?php endif; ?>
-    <li style="float:right"><a href="<?php echo base_url() ?>/signout">Sign Out</a></li>
-    <li style="float:right"><a href="javascript:;" class="static"><?php echo auth()->user()->firstname . ' ' . auth()->user()->lastname ?></a></li>
-</ul>
