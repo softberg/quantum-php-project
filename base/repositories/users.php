82a0c31af1054d0f2e2b2efa0c66efd6a7bb47ec
Artak <artakmanukyan17@gmail.com>
2021-02-11 11:30:12 +0400

    Two step verification feature(more changes)

--- base/repositories/users.php
+++ base/repositories/users.php
@@ -12,25 +12,9 @@ return array (
     'remember_token' => '',
     'reset_token' => '',
     'access_token' => '',
-    'refresh_token' => 'JDJ5JDEyJGdPSzJOd2RDcGZjREVJelJXa0VIWi5vU0NOLjFyMmk2VHpqeC9meFZQc0hZdkNJM2EvM1dl',
+    'refresh_token' => '',
     'otp' => '',
     'otp_expiry_in' => '',
     'otp_token' => '',
-  ),
-  2 => 
-  array (
-    'email' => 'artakmanukyan17@gmail.com',
-    'firstname' => '1234123',
-    'lastname' => '1234123',
-    'role' => '',
-    'password' => '$2y$12$lFYqXAvGn07T4.ESAV6.FupuxEO7CJVjZTmemuboDbycc56PxLcli',
-    'activation_token' => '',
-    'remember_token' => '',
-    'reset_token' => '',
-    'access_token' => '',
-    'refresh_token' => 'JDJ5JDEyJDNxSVh0ejlscEx1SjF3R3RMbFZ3MU91Lks5bXJPdjVlZnNNWER1bUpub1Riak5VWGNISjE2',
-    'otp' => 935679,
-    'otp_expiry_in' => '2021-02-10 13:42',
-    'otp_token' => 'JDJ5JDEyJFNnbVRuby9tRmRDck91dHpjOENCZi55M1RnR2o4VE9UM21BdElZTmw3ZEoydGl2eGNranBT',
-  ),
+  )
 );
