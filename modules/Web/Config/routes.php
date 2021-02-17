dc7404706f55e4a1c04ddb8623127bf76dd8c1d6
Artak <artakmanukyan17@gmail.com>
2021-02-17 12:04:55 +0400

    Routes in project

--- modules/Web/Config/routes.php
+++ modules/Web/Config/routes.php
@@ -1,24 +1,24 @@
 <?php
 
 return function ($route) {
-    $route->add('[:alpha:2]?', 'GET', 'MainController', 'index');
-    $route->add('[:alpha:2]?/about', 'GET', 'MainController', 'about');
+    $route->get('[:alpha:2]?', 'MainController', 'index');
+    $route->get('[:alpha:2]?/about', 'MainController', 'about');
 
     $route->group('guest', function ($route) {
         $route->add('[:alpha:2]?/signin', 'GET|POST', 'AuthController', 'signin');
         $route->add('[:alpha:2]?/signup', 'GET|POST', 'AuthController', 'signup')->middlewares(['Signup']);
-        $route->add('[:alpha:2]?/activate/[:any]', 'GET', 'AuthController', 'activate')->middlewares(['Activate']);
+        $route->get('[:alpha:2]?/activate/[:any]', 'AuthController', 'activate')->middlewares(['Activate']);
         $route->add('[:alpha:2]?/forget', 'GET|POST', 'AuthController', 'forget')->middlewares(['Forget']);
         $route->add('[:alpha:2]?/reset/[:any]', 'GET|POST', 'AuthController', 'reset')->middlewares(['Reset']);
         $route->add('[:alpha:2]?/verify/[:any]?', 'GET|POST', 'AuthController', 'verify')->middlewares(['Verify']);
-        $route->add('[:alpha:2]?/resend/[:any]?', 'GET', 'AuthController', 'resend');
+        $route->get('[:alpha:2]?/resend/[:any]?', 'AuthController', 'resend');
     })->middlewares(['Guest']);
 
     $route->group('auth', function ($route) {
-        $route->add('[:alpha:2]?/signout', 'GET', 'AuthController', 'signout');
-        $route->add('[:alpha:2]?/posts', 'GET', 'PostController', 'getPosts');
-        $route->add('[:alpha:2]?/post/[:num]', 'GET', 'PostController', 'getPost');
+        $route->get('[:alpha:2]?/signout', 'AuthController', 'signout');
+        $route->get('[:alpha:2]?/posts', 'PostController', 'getPosts');
+        $route->get('[:alpha:2]?/post/[:num]', 'PostController', 'getPost');
         $route->add('[:alpha:2]?/post/amend/[:num]?', 'GET|POST|PUT', 'PostController', 'amendPost')->middlewares(['Editor']);
-        $route->add('[:alpha:2]?/post/delete/[:num]', 'GET', 'PostController', 'deletePost')->middlewares(['Editor']);
+        $route->get('[:alpha:2]?/post/delete/[:num]', 'PostController', 'deletePost')->middlewares(['Editor']);
     })->middlewares(['Auth']);
 };
