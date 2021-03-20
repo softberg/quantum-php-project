515a42fb9d47ef6eddb18ecd2875057a43b50343
Arman <armanist84@mail.ru>
2021-03-20 15:12:14 +0400

    Loader as injectable dependency

--- base/Services/AuthService.php
+++ base/Services/AuthService.php
@@ -41,9 +41,10 @@ class AuthService extends QtService implements AuthServiceInterface
 
     /**
      * Init
+     * @param Loader $loader
      * @throws \Exception
      */
-    public function __init()
+    public function __init(Loader $loader)
     {
         $loaderSetup = (object) [
                     'module' => current_module(),
@@ -52,7 +53,7 @@ class AuthService extends QtService implements AuthServiceInterface
                     'exceptionMessage' => ExceptionMessages::CONFIG_FILE_NOT_FOUND
         ];
 
-        self::$users = (new Loader())->setup($loaderSetup)->load();
+        self::$users = $loader->setup($loaderSetup)->load();
     }
 
     /**
