d80317af80374771a10f2ec5ca4340afcf0de724
Arman <arman.aghabekyan@gmail.com>
2021-04-17 20:02:21 +0400

    Updating user record according a way of new var export

--- base/repositories/users.php
+++ base/repositories/users.php
@@ -1,20 +1,19 @@
 <?php
 
-return array (
-  1 => 
-  array (
-    'email' => 'admin@qt.com',
-    'firstname' => 'Admin',
-    'lastname' => 'User',
-    'role' => 'admin',
-    'password' => '$2y$12$P7lpP9LZpq5EdAnmP1m9JearM0Bd32FmHR0wlnUdN8WcTmBaIJhpq',
-    'activation_token' => '',
-    'remember_token' => '',
-    'reset_token' => '',
-    'access_token' => '',
-    'refresh_token' => '',
-    'otp' => '',
-    'otp_expires' => '',
-    'otp_token' => '',
-  ),
-);
+return [
+    1 => [
+        'email' => 'admin@qt.com',
+        'firstname' => 'Admin',
+        'lastname' => 'Hunter',
+        'role' => 'admin',
+        'password' => '$2y$12$4Y4/1a4308KEiGX/xo6vgO41szJuDHC7KhpG5nknx/xxnLZmvMyGi',
+        'activation_token' => '',
+        'remember_token' => '',
+        'reset_token' => '',
+        'access_token' => '',
+        'refresh_token' => '',
+        'otp' => '',
+        'otp_expires' => '',
+        'otp_token' => '',
+    ],
+];
