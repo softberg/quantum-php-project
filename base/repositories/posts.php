62fe32535a5b05210880da9b78fc6e5ef089c9d5
Arman <arman.aghabekyan@gmail.com>
2021-04-17 20:01:42 +0400

    Updating default posts

--- base/repositories/posts.php
+++ base/repositories/posts.php
@@ -1,14 +1,12 @@
 <?php
 
-return array (
-  1 => 
-  array (
-    'title' => 'Lorem ipsum dolor sit amet',
-    'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu odio vel ligula cursus blandit. Fusce eu eros a felis aliquet sodales. Phasellus porttitor nisl urna, eu dignissim enim egestas sed. ',
-  ),
-  2 => 
-  array (
-    'title' => 'Curabitur feugiat ultrices congue',
-    'content' => 'Curabitur feugiat ultrices congue. Etiam pellentesque urna vitae tincidunt pulvinar. Proin ultricies libero mi, in mattis ex iaculis a. Nullam lectus nulla, sodales sit amet faucibus quis, efficitur in elit.',
-  ),
-);
+return [
+    1 => [
+        'title' => 'Walt Disney',
+        'content' => 'The way to get started is to quit talking and begin doing.',
+    ],
+    [
+        'title' => 'James Cameron',
+        'content' => 'If you set your goals ridiculously high and it&#39;s a failure, you will fail above everyone else&#39;s success.',
+    ],
+];
