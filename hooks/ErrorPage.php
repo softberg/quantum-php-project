af6e3d9a8a4f89cd9daca7d07ce6d4c0ae057d1b
Arman <armanist84@mail.ru>
2021-08-26 21:47:55 +0400

    Removing the argument of the pageNotFound() method

--- hooks/ErrorPage.php
+++ hooks/ErrorPage.php
@@ -9,7 +9,7 @@
  * @author Arman Ag. <arman.ag@softberg.org>
  * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
  * @link http://quantum.softberg.org/
- * @since 2.0.0
+ * @since 2.5.0
  */
 
 namespace Hooks;
@@ -17,6 +17,7 @@ namespace Hooks;
 use Quantum\Factory\ViewFactory;
 use Quantum\Hooks\HookInterface;
 use Quantum\Http\Response;
+use Quantum\Di\Di;
 
 /**
  * Class ErrorPage
@@ -24,14 +25,17 @@ use Quantum\Http\Response;
  */
 class ErrorPage implements HookInterface
 {
-
     /**
      * Page not found
-     *
-     * @throws \Exception
+     * @throws \Quantum\Exceptions\DiException
+     * @throws \Quantum\Exceptions\HookException
+     * @throws \Quantum\Exceptions\StopExecutionException
+     * @throws \Quantum\Exceptions\ViewException
+     * @throws \ReflectionException
      */
-    public static function pageNotFound(Response $response)
+    public static function pageNotFound()
     {
+        $response = Di::get(Response::class);
         $view = (new ViewFactory())->renderPartial('errors/404');
         $response->html($view);
         stop();
