81dfafb9a469fc456f0af1f7404119390fdd60fc
Arman <armanist84@mail.ru>
2021-12-24 15:53:28 +0400

    Asset class related changes

--- modules/Web/Views/layouts/main.php
+++ modules/Web/Views/layouts/main.php
@@ -6,9 +6,9 @@
         <title><?php echo $title ?></title>
 
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="shortcut icon" href="<?php echo base_url() ?>/assets/images/favicon.ico">
-        <link rel='stylesheet' href='<?php echo asset('css/materialize.min.css') ?>' type='text/css' media='screen,projection' />
-        <link rel='stylesheet' href='<?php echo asset('css/custom.css') ?>' type='text/css' />
+        <link rel="shortcut icon" href="<?php echo asset()->url('images/favicon.ico') ?>">
+        <link rel='stylesheet' href='<?php echo asset()->url('css/materialize.min.css') ?>' type='text/css' media='screen,projection' />
+        <link rel='stylesheet' href='<?php echo asset()->url('css/custom.css') ?>' type='text/css' />
     </head>
     <body>
         <header>
@@ -25,7 +25,7 @@
         <?php echo debugbar() ?>
 
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script type='text/javascript' src='<?php echo asset('js/materialize.min.js') ?>'></script>
-        <script type='text/javascript' src='<?php echo asset('js/custom.js') ?>'></script>
+        <script type='text/javascript' src='<?php echo asset()->url('js/materialize.min.js') ?>'></script>
+        <script type='text/javascript' src='<?php echo asset()->url('js/custom.js') ?>'></script>
     </body>
 </html>
