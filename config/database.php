a65e21a148d2cf40f725eb48f41b0de21cef453f
Arman <armanist84@mail.ru>
2021-12-27 13:06:16 +0400

    Sleekdb db default config section

--- config/database.php
+++ config/database.php
@@ -9,7 +9,7 @@ return [
      * Current configuration to use.
      */
     'current' => 'mysql',
-    
+
     /**
      * ---------------------------------------------------------
      * Database Connections
@@ -17,25 +17,38 @@ return [
      *
      * You can define as many database configurations as you want.
      *
-     * driver     : mysql, pgsql, sqlite
+     * driver     : mysql, pgsql, sqlite, sleekdb
      * host       : The database server (localhost)
      * dbname     : The database name
      * username   : Username of the database server
      * password   : Password of the database server
      * charset    : Default charset
      */
-    'mysql' => array(
+    'mysql' => [
         'driver' => 'mysql',
         'host' => 'localhost',
         'dbname' => 'database',
         'username' => 'username',
         'password' => 'password',
         'charset' => 'charset',
-    ),
-    'sqlite' => array(
+    ],
+    'sqlite' => [
         'driver' => 'sqlite',
         'database' => 'database.sqlite',
         'prefix' => '',
-    ),
+    ],
+    'sleekdb' => [
+        'driver' => 'sleekdb',
+        "auto_cache" => true,
+        "cache_lifetime" => null,
+        "timeout" => false,
+        "primary_key" => "_id",
+        "search" => [
+            "min_length" => 2,
+            "mode" => "or",
+            "score_key" => "scoreKey",
+            "algorithm" => Query::SEARCH_ALGORITHM["hits"]
+        ]
+    ]
 ];
 
