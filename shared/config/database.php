ba1e4758ead3f47af52aaefa41949b9711706ad8
Arman <armanist84@mail.ru>
2022-06-09 13:04:03 +0400

    Making the sleekdb as default config

--- shared/config/database.php
+++ shared/config/database.php
@@ -8,7 +8,7 @@ return [
      *
      * Current configuration to use.
      */
-    'current' => 'mysql',
+    'current' => 'sleekdb',
 
     /**
      * ---------------------------------------------------------
