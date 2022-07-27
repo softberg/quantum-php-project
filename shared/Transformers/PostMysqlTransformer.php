c513ca133ccbcd0a0c4e3eccdfee5bc77eb28db0
Arman <armanist84@mail.ru>
2022-07-27 19:13:21 +0400

    Post Transformers

---
+++ shared/Transformers/PostMysqlTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.8.0
+ */
+
+namespace Shared\Transformers;
+
+use Quantum\Libraries\Transformer\TransformerInterface;
+
+/**
+ * Class PostSleekTransformer
+ * @package Shared\Transformers
+ */
+class PostMysqlTransformer implements TransformerInterface
+{
+
+    public function transform($item)
+    {
+       //
+    }
+
+}
---
+++ shared/Transformers/PostSleekTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.8.0
+ */
+
+namespace Shared\Transformers;
+
+use Quantum\Libraries\Transformer\TransformerInterface;
+
+/**
+ * Class PostSleekTransformer
+ * @package Shared\Transformers
+ */
+class PostSleekTransformer implements TransformerInterface
+{
+
+    public function transform($item)
+    {
+        return [
+            'id' => $item['uuid'],
+            'title' => $item['title'],
+            'content' => $item['content'],
+            'image' => $item['image'],
+            'date' => date('Y/m/d H:i', strtotime($item['updated_at'])),
+            'author' => $item['users'][0]['firstname'] . ' ' . $item['users'][0]['lastname']
+        ];
+    }
+
+}
