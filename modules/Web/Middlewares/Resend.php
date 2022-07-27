e7b775b3b99467dbfc8cd1a69c29aa584a5feaf8
Arman <armanist84@mail.ru>
2022-07-27 12:06:04 +0400

    New middleware for resend otp

---
+++ modules/Web/Middlewares/Resend.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.8.0
+ */
+
+namespace Modules\Web\Middlewares;
+
+use Quantum\Middleware\QtMiddleware;
+use Quantum\Http\Response;
+use Quantum\Http\Request;
+use Closure;
+
+/**
+ * Class Resend
+ * @package Modules\Web\Middlewares
+ */
+class Resend extends QtMiddleware
+{
+
+    /**
+     * @param \Quantum\Http\Request $request
+     * @param \Quantum\Http\Response $response
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function apply(Request $request, Response $response, Closure $next)
+    {
+        if (!route_param('code')) {
+            redirect(base_url() . '/' . current_lang() . '/signin');
+        }
+
+        return $next($request, $response);
+    }
+
+}
