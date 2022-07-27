b9d15de932efec0a728e4fa7b4b4651a1872559f
Arman <armanist84@mail.ru>
2022-07-27 12:08:46 +0400

    Updating routes in relates to named route params and correcting the middlewares

--- modules/Api/Config/routes.php
+++ modules/Api/Config/routes.php
@@ -4,22 +4,22 @@ return function ($route) {
 
     $route->post('[:alpha:2]?/api-signin', 'AuthController', 'signin');
     $route->post('[:alpha:2]?/api-signup', 'AuthController', 'signup')->middlewares(['Signup']);
-    $route->get('[:alpha:2]?/api-activate/[:any]', 'AuthController', 'activate')->middlewares(['Activate']);
     $route->post('[:alpha:2]?/api-forget', 'AuthController', 'forget')->middlewares(['Forget']);
-    $route->post('[:alpha:2]?/api-reset/[:any]', 'AuthController', 'reset')->middlewares(['Reset']);
+    $route->get('[:alpha:2]?/api-activate/[token=:any]', 'AuthController', 'activate')->middlewares(['Activate']);
+    $route->post('[:alpha:2]?/api-reset/[token=:any]', 'AuthController', 'reset')->middlewares(['Reset']);
+    $route->get('[:alpha:2]?/api-resend/[code=:any]', 'AuthController', 'resend')->middlewares(['Resend']);
     $route->post('[:alpha:2]?/api-verify', 'AuthController', 'verify')->middlewares(['Verify']);
-    $route->post('[:alpha:2]?/api-resend', 'AuthController', 'resend')->middlewares(['Resend']);
 
     $route->get('[:alpha:2]?/api-posts', 'PostController', 'getPosts');
-    $route->get('[:alpha:2]?/api-posts/[:any]', 'PostController', 'getPost');
+    $route->get('[:alpha:2]?/api-post/[id=:any]', 'PostController', 'getPost')->middlewares(['Post']);
 
     $route->group('auth', function ($route) {
-        $route->get('[:alpha:2]?/api-signout', 'AuthController', 'signout');
+        $route->get('[:alpha:2]?/api-signout', 'AuthController', 'signout')->middlewares(['Signout']);
         $route->get('[:alpha:2]?/api-my-posts', 'PostController', 'getMyPosts')->middlewares(['Editor']);
         $route->post('[:alpha:2]?/api-my-posts/create', 'PostController', 'createPost')->middlewares(['Editor']);
-        $route->add('[:alpha:2]?/api-my-posts/amend/[:any]', 'PUT', 'PostController', 'amendPost')->middlewares(['Editor']);
-        $route->add('[:alpha:2]?/api-my-posts/delete/[:any]', 'DELETE', 'PostController', 'deletePost')->middlewares(['Editor']);
-        $route->add('[:alpha:2]?/api-my-posts/delete-image/[:any]', 'DELETE', 'PostController', 'deletePostImage')->middlewares(['Editor']);
+        $route->add('[:alpha:2]?/api-my-posts/amend/[id=:any]', 'PUT', 'PostController', 'amendPost')->middlewares(['Editor', 'Owner']);
+        $route->add('[:alpha:2]?/api-my-posts/delete/[id=:any]', 'DELETE', 'PostController', 'deletePost')->middlewares(['Editor', 'Owner']);
+        $route->add('[:alpha:2]?/api-my-posts/delete-image/[id=:any]', 'DELETE', 'PostController', 'deletePostImage')->middlewares(['Editor', 'Owner']);
     })->middlewares(['Auth']);
     
 };
