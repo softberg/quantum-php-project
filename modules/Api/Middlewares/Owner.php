6a6f3235f7a081778c1182428b99be5c2fb33dbf
Arman <armanist84@mail.ru>
2022-07-27 11:59:56 +0400

    New middlewares for checking the ownership of the post

---
+++ modules/Api/Middlewares/Owner.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.8.0
+ */
+
+namespace Modules\Api\Middlewares;
+
+use Quantum\Middleware\QtMiddleware;
+use Quantum\Factory\ServiceFactory;
+use Shared\Services\PostService;
+use Quantum\Http\Response;
+use Quantum\Http\Request;
+use Closure;
+
+/**
+ * Class Editor
+ * @package Modules\Web\Middlewares
+ */
+class Owner extends QtMiddleware
+{
+
+    /**
+     * @param \Quantum\Http\Request $request
+     * @param \Quantum\Http\Response $response
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function apply(Request $request, Response $response, Closure $next)
+    {
+        $postId = route_param('id');
+
+        $post = ServiceFactory::get(PostService::class)->getPost($postId, false);
+
+        if (!$post || $post['user_id'] != auth()->user()->getFieldValue('id')) {
+            $response->json([
+                'status' => 'error',
+                'message' => t('common.post_not_found')
+            ], 404);
+
+            stop();
+        }
+
+        return $next($request, $response);
+    }
+
+}
