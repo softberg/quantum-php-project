af1203faf64525aeb63d35cce7345fa0b0aad4a2
Arno <arno2005petrosyan@gmail.com>
2024-07-26 19:21:24 +0400

    Install default modules by command

--- modules/Web/Views/partials/messages/success.php
+++
@@ -1,4 +0,0 @@
-<div class="material-alert success left-align" role="alert">
-    <?php $success = session()->getFlash('success'); ?>
-    <?php echo $success; ?>
-</div>
--- modules/Web/Views/post/partials/back.php
+++
@@ -1,3 +0,0 @@
-<a class="back-btn" title="Back" href="<?php echo  get_referrer() ?: base_url(true) . '/' . current_lang() . '/posts'; ?>">
-    <i class="material-icons">arrow_back</i>
-</a>
