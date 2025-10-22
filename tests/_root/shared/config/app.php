5124f42b07a15596dfc6bee4baffa2f5c27bce49
Arman <407448+armanist@users.noreply.github.com>
2025-10-22 16:41:45 +0400

    Updating test cases related to config renaming

---
+++ tests/_root/shared/config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => 'Quantum PHP Framework',
+    'email' => 'app@quantumphp.io',
+    'version' => '2.9.5',
+    'key' => env('APP_KEY'),
+    'base_url' => 'http://localhost',
+    'debug' => false,
+    'test' => 'Testing',
+];
--- tests/_root/shared/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'app_key' => env('APP_KEY'),
-    'app_email' => 'app@quantumphp.io',
-    'debug' => true,
-    'test' => 'Testing',
-    'otp_expires' => 2,
-];
