64a992a31402cb435d659c2d393afa237ebca711
Arman <407448+armanist@users.noreply.github.com>
2025-12-24 16:13:22 +0400

    Removing outdated argument

--- shared/Commands/DemoCommand.php
+++ shared/Commands/DemoCommand.php
@@ -228,7 +228,7 @@ class DemoCommand extends QtCommand
             '--yes' => true,
             '--template' => $template,
             '--with-assets' => $withAssets
-        ], true);
+        ]);
     }
 
     /**
